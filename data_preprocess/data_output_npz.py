b633c9ac34ebe394143b1701151359e1010d4c54
AlexsaseXie <thss15_xieyw@163.com>
2019-04-11 13:20:20 +0800

    output rendered img to .npz

---
+++ data_preprocess/data_output_npz.py
@@ -0,0 +1,70 @@
+import os
+import numpy as np
+from skimage import io
+
+NUM_VIEWS = 12
+DEGREE = 360 // NUM_VIEWS
+ELEVATION = ['upside','downside']
+
+IDS_ROOT = 'ids/'
+CLASS_IDS_ALL = (
+    '02691156,02828884,02933112,02958343,03001627,03211117,03636649,' +
+    '03691459,04090263,04256520,04379243,04401088,04530566')
+CLASSES = CLASS_IDS_ALL.split(',')
+
+TYPE = ['val','test','train']
+
+DATASET_ROOT = '../data/dataset/'
+SHAPENET_ROOT = '/home4/data/xieyunwei/ShapeNetCore.v2/'
+
+f = open(os.path.join(IDS_ROOT,'missing.txt'),'w')
+
+for c in CLASSES:
+    for t in TYPE:
+        f_ids = open(os.path.join(IDS_ROOT,'%s_%s_ids.txt' % (c,t)))
+        f_ids_all = f_ids.readlines()
+
+        dataset_images = []
+
+        for line in f_ids_all:
+            if line[-1] == '\n':
+                line = line[:-1]
+            
+            if len(line) <= 5:
+                break
+
+            class_id = c
+            model_id = line.split('/')[1]
+            
+            model_root = os.path.join(SHAPENET_ROOT,class_id,model_id)
+            render_root = os.path.join(model_root,'render','render_upside_000.png')
+
+            if not os.path.exists(render_root):
+                continue
+            
+            images = []
+            for side in ELEVATION:
+                for i in range(NUM_VIEWS):
+                    azimuth = i * DEGREE
+                    png_root = os.path.join(model_root,'render','render_{s}_{0:03d}'.format(side, int(azimuth)))
+
+                    img = io.imread(png_root)
+                    assert img.shape[2] == 4
+
+                    images.append(img.transpose(2,0,1))
+            
+            images = np.stack(images, axis = 0)
+            assert images.shape[0] == 24
+
+            dataset_images.append(images)
+        
+        dataset_images = np.stack(dataset_images , axis = 0)
+        dataset_images = ('%s_%s' % (c,t), dataset_images)
+        dataset_images = np.stack([dataset_images], axis = 0)
+
+        np.save(os.path.join(DATASET_ROOT,'%s_%s_render.npz'), dataset_images)
+    
+
+
+
+                       
