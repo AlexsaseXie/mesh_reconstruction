b80d92fb86ba4e4f3447a130f36c2599c2391219
AlexsaseXie <thss15_xieyw@163.com>
2019-05-07 12:20:13 +0800

    new dataset

--- mesh_reconstruction/datasets.py
+++ mesh_reconstruction/datasets.py
@@ -127,6 +127,142 @@ class ShapeNet_NView(object):
         return self.model_count
 
 
+class ShapeNet_NView_Gan(object):
+    def __init__(self, directory=None, class_ids=None, set_name=None, n_views = 3, total_views=24, only_top=True):
+        self.class_ids = class_ids
+        self.set_name = set_name
+        self.elevation = 30.964
+        self.distance = 1.1662
+        self.n_views = n_views
+        self.total_views = total_views
+
+        self.only_top = only_top
+
+        images = []
+        voxels = []
+        self.num_data = {}
+        self.pos = {}
+        count = 0
+        loop = tqdm.tqdm(self.class_ids)
+        loop.set_description('Loading dataset')
+        for class_id in loop:
+            images.append(np.load(
+                os.path.join(directory, '%s_%s_render.npz' % (class_id, set_name))).items()[0][1])
+            voxels.append(np.load(
+                os.path.join(directory, '%s_%s_voxel.npz' % (class_id, set_name))).items()[0][1])
+            self.num_data[class_id] = images[-1].shape[0]
+            self.pos[class_id] = count
+            count += self.num_data[class_id]
+        images = np.concatenate(images, axis=0).reshape((-1, 4, 128, 128))
+        images = np.ascontiguousarray(images)
+        self.images = images
+        self.voxels = np.ascontiguousarray(np.concatenate(voxels, axis=0))
+        self.model_count = images.shape[0] // 24
+
+        self.init_viewpoints()
+        del images
+        del voxels
+
+    def init_viewpoints(self):
+        if not self.only_top:
+            distance = np.ones(self.total_views, 'float32') * self.distance
+            elevation = np.concatenate((np.ones(self.total_views / 2, 'float32') * self.elevation, - np.ones(self.total_views / 2, 'float') * self.elevation) , axis = 0)
+            azimuth = np.arange(0, self.total_views, dtype='int32') * (360 / (self.total_views / 2))
+            self.viewpoints_pool = neural_renderer.get_points_from_angles(distance, elevation, azimuth)
+        else:
+            distance = np.ones(self.total_views, 'float32') * self.distance
+            elevation = np.ones(self.total_views, 'float32') * self.elevation
+            azimuth = np.arange(0, self.total_views, dtype='int32') * (360 / self.total_views)
+            self.viewpoints_pool = neural_renderer.get_points_from_angles(distance, elevation, azimuth)
+
+        
+    def get_random_batch(self, batch_size):
+        data_ids = np.zeros((batch_size, self.n_views), 'int32')
+        viewpoint_ids = np.zeros((batch_size, self.n_views), 'int32')
+        gan_data_ids = np.zeros((batch_size) , 'int32')
+        gan_viewpoint_ids = np.zeros((batch_size), 'int32')
+        for i in range(batch_size):
+            class_id = np.random.choice(self.class_ids)
+            object_id = np.random.randint(0, self.num_data[class_id])
+
+            r_id = random.sample(range(0, self.total_views), self.n_views + 1)
+            for j in range(self.n_views):
+                viewpoint_id = r_id[j]
+                data_id = (object_id + self.pos[class_id]) * self.total_views + viewpoint_id
+                data_ids[i,j] = data_id
+                viewpoint_ids[i,j] = viewpoint_id
+
+            gan_viewpoint_ids[i] = r_id[self.n_views]
+            gan_data_ids[i] = (object_id + self.pos[class_id]) * self.total_views + gan_viewpoint_ids[i] 
+
+        images = self.images[data_ids].astype('float32') / 255.
+        real_images = self.images[gan_data_ids].astype('float32') / 255
+
+        #viewpoints = []
+        #distances = np.ones(self.n_views, 'float32') * self.distance
+        #for i in range(batch_size):
+        #    elevations_tmp = np.ones(self.n_views, 'float32') * self.elevation * ((viewpoint_ids[i,:] // 12) * 2 - 1)
+        #    viewpoints_tmp = neural_renderer.get_points_from_angles(distances, elevations_tmp, -viewpoint_ids[i,:] * 30)
+        #    viewpoints.append(viewpoints_tmp)
+
+        #viewpoints = np.stack(viewpoints, axis=0)
+        viewpoints = self.viewpoints_pool[viewpoint_ids]
+        real_images_viewpoints = self.viewpoints_pool[gan_viewpoint_ids]
+        return images, viewpoints, real_images, real_images_viewpoints
+
+    def get_all_batches_for_evaluation(self, batch_size, class_id):
+        data_ids = np.arange(self.num_data[class_id]) + self.pos[class_id]
+        viewpoint_ids = np.tile(np.arange(self.total_views), data_ids.size)
+        data_ids = np.repeat(data_ids, self.total_views) * self.total_views + viewpoint_ids
+        for i in range((data_ids.size - 1) / batch_size + 1):
+            images = self.images[data_ids[i * batch_size:(i + 1) * batch_size]].astype('float32') / 255.
+            voxels = self.voxels[data_ids[i * batch_size:(i + 1) * batch_size] / self.total_views]
+            yield images, voxels
+
+    def __getitem__(self, index):
+        #print index
+        if isinstance(index, list) or isinstance(index, np.ndarray):
+            batch_size = len(index)
+            data_ids = np.zeros((batch_size, self.n_views), 'int32')
+            viewpoint_ids = np.zeros((batch_size, self.n_views), 'int32')
+            
+            gan_data_ids = np.zeros((batch_size) , 'int32')
+            gan_viewpoint_ids = np.zeros((batch_size), 'int32')
+            for i in range(batch_size):
+                r_id = random.sample(range(0,self.total_views), self.n_views + 1)
+                for j in range(self.n_views):
+                    viewpoint_id = r_id[j]
+                    data_id = index[i] * self.total_views + viewpoint_id
+                    data_ids[i,j] = data_id
+                    viewpoint_ids[i,j] = viewpoint_id
+
+                gan_viewpoint_ids[i] = r_id[self.n_views]
+                gan_data_ids[i] = index[i] * self.total_views + gan_viewpoint_ids[i] 
+
+            images = self.images[data_ids].astype('float32') / 255.
+            real_images = self.images[gan_data_ids].astype('float32') / 255
+
+            #distances = np.ones(self.n_views, 'float32') * self.distance
+            #for i in range(batch_size):
+            #    elevations_tmp = np.ones(self.n_views, 'float32') * self.elevation * ((viewpoint_ids[i,:] // 12) * 2 - 1)
+            #    viewpoints_tmp = neural_renderer.get_points_from_angles(distances, elevations_tmp, -viewpoint_ids[i,:] * 30)
+            #    viewpoints.append(viewpoints_tmp)
+        
+            #viewpoints = np.stack(viewpoints, axis=0)
+            viewpoints = self.viewpoints_pool[viewpoint_ids]
+            real_images_viewpoints = self.viewpoints_pool[gan_viewpoint_ids]
+            return images, viewpoints, real_images, real_images_viewpoints
+
+        elif isinstance(index, slice):
+            current, stop, step = index.indices(len(self))
+            return self[range(current,stop,step)]
+        else:
+            raise NotImplementedError
+
+    def __len__(self):
+        return self.model_count
+
+
 if __name__ == '__main__':
     CLASS_IDS_ALL = (
     '03211117')
---
+++ mesh_reconstruction/train_gan.py
@@ -0,0 +1,105 @@
+import argparse
+import functools
+import os
+import random
+
+import chainer
+import cupy as cp
+import neural_renderer
+import numpy as np
+
+import datasets
+import models
+import model_nview
+import training
+import gan_updater
+
+BATCH_SIZE = 32
+LEARNING_RATE = 1e-4
+LR_REDUCE_POINT = 0.95
+NUM_ITERATIONS = 1000000
+CLASS_IDS_ALL = (
+    '02691156,02828884,02933112,02958343,03001627,03211117,03636649,' +
+    '03691459,04090263,04256520,04379243,04401088,04530566')
+LAMBDA_SMOOTHNESS = 0
+LAMBDA_STD = 0.01
+N_VIEWS = 3
+
+LOG_INTERVAL = 10000
+RANDOM_SEED = 0
+GPU = 0
+MODEL_DIRECTORY = './data/models'
+DATASET_DIRECTORY = './data/dataset'
+
+
+def run():
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-eid', '--experiment_id', type=str)
+    parser.add_argument('-d', '--model_directory', type=str, default=MODEL_DIRECTORY)
+    parser.add_argument('-dd', '--dataset_directory', type=str, default=DATASET_DIRECTORY)
+    parser.add_argument('-dv', '--dataset_views', type=int, default=24)
+    parser.add_argument('-cls', '--class_ids', type=str, default=CLASS_IDS_ALL)
+    parser.add_argument('-bs', '--batch_size', type=int, default=BATCH_SIZE)
+    parser.add_argument('-ls', '--lambda_smoothness', type=float, default=LAMBDA_SMOOTHNESS)
+    parser.add_argument('-lr', '--learning_rate', type=float, default=LEARNING_RATE)
+    parser.add_argument('-lrp', '--lr_reduce_point', type=float, default=LR_REDUCE_POINT)
+    parser.add_argument('-ni', '--num_iterations', type=int, default=NUM_ITERATIONS)
+    parser.add_argument('-li', '--log_interval', type=int, default=LOG_INTERVAL)
+    parser.add_argument('-s', '--seed', type=int, default=RANDOM_SEED)
+    parser.add_argument('-g', '--gpu', type=int, default=GPU)
+    parser.add_argument('-c','--con',type=bool, default=False)
+    parser.add_argument('-lstd','--lambda_std',type=float, default=LAMBDA_STD)
+    parser.add_argument('-nviews','--n_views', type=int, default=N_VIEWS)
+    args = parser.parse_args()
+    directory_output = os.path.join(args.model_directory, args.experiment_id)
+
+    # set random seed, gpu
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    cp.random.seed(args.seed)
+    chainer.cuda.get_device(args.gpu).use()
+
+    # load dataset
+    dataset_train = datasets.ShapeNet_NView(args.dataset_directory, args.class_ids.split(','), 'train', n_views=args.n_views, total_views=args.dataset_views)
+    dataset_val = datasets.ShapeNet_NView(args.dataset_directory, args.class_ids.split(','), 'val', total_views=args.dataset_views)
+    train_iter = training.M_SerialIterator(dataset_train, args.batch_size)
+
+    # setup model & optimizer
+    model = model_nview.Model(lambda_smoothness=args.lambda_smoothness,lambda_std=args.lambda_std,n_views=args.n_views)
+    model.to_gpu()
+    if args.con:
+        print 'loading pretrained model'
+        chainer.serializers.load_npz(os.path.join(directory_output, 'model.npz'), model)
+
+    optimizer = neural_renderer.Adam(args.learning_rate)
+    optimizer.setup(model)
+
+    # setup trainer
+    updater = chainer.training.StandardUpdater(train_iter, optimizer, converter=training.my_convertor)
+    #updater = chainer.training.StandardUpdater(train_iter, optimizer)
+    trainer = chainer.training.Trainer(updater, stop_trigger=(args.num_iterations, 'iteration'), out=directory_output)
+    trainer.extend(chainer.training.extensions.LogReport(trigger=(args.log_interval, 'iteration')))
+    trainer.extend(chainer.training.extensions.PrintReport(
+        ['iteration', 'main/loss_silhouettes', 'main/loss_smoothness', 'val/iou', 'elapsed_time']))
+    trainer.extend(chainer.training.extensions.ProgressBar(update_interval=10))
+    trainer.extend(
+        functools.partial(training.validation, model=model, dataset=dataset_val),
+        name='validation',
+        priority=chainer.training.PRIORITY_WRITER,
+        trigger=(args.log_interval, 'iteration'))
+    trainer.extend(
+        functools.partial(
+            training.lr_shift, optimizer=optimizer, iterations=[args.num_iterations * args.lr_reduce_point]),
+        name='lr_shift',
+        trigger=(1, 'iteration'))
+
+    # main loop
+    trainer.run()
+
+    # save model
+    chainer.serializers.save_npz(os.path.join(directory_output, 'model.npz'), model)
+
+
+if __name__ == '__main__':
+    run()
